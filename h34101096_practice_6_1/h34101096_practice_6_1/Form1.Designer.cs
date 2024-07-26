754e18967fcdd03cfeebac74d0ea68c5b1abfdfb
Jason5789 <109019020+Jason5789@users.noreply.github.com>
2024-07-26 21:49:51 +0800

    Add files via upload

---
+++ h34101096_practice_6_1/h34101096_practice_6_1/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace h34101096_practice_6_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bpm = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bpm
+            // 
+            this.bpm.AutoSize = true;
+            this.bpm.Font = new System.Drawing.Font("新細明體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.bpm.Location = new System.Drawing.Point(381, 9);
+            this.bpm.Name = "bpm";
+            this.bpm.Size = new System.Drawing.Size(251, 60);
+            this.bpm.TabIndex = 0;
+            this.bpm.Text = "120 BPM";
+            this.bpm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton1.Location = new System.Drawing.Point(261, 225);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(16, 15);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.radioButton2.Location = new System.Drawing.Point(544, 225);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(17, 16);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(177, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 56);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(400, 157);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(57, 56);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Visible = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(638, 157);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(57, 56);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Visible = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(858, 157);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(57, 56);
+            this.button8.TabIndex = 6;
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(92, 82);
+            this.trackBar1.Maximum = 180;
+            this.trackBar1.Minimum = 60;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(812, 56);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Value = 120;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(245, 535);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 38);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Beat Emphasis";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("新細明體", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4",
+            "8"});
+            this.comboBox1.Location = new System.Drawing.Point(509, 535);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(274, 45);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(72, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 56);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(284, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 56);
+            this.button3.TabIndex = 11;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(520, 157);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(57, 56);
+            this.button5.TabIndex = 12;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Visible = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(752, 157);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(57, 56);
+            this.button7.TabIndex = 13;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Visible = false;
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.start.Location = new System.Drawing.Point(431, 629);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(159, 44);
+            this.start.TabIndex = 14;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button11.Enabled = false;
+            this.button11.Location = new System.Drawing.Point(339, 157);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(57, 56);
+            this.button11.TabIndex = 17;
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button12.Enabled = false;
+            this.button12.Location = new System.Drawing.Point(581, 157);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(57, 56);
+            this.button12.TabIndex = 18;
+            this.button12.UseVisualStyleBackColor = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(46, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 30);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "60";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(900, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 30);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "180";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 741);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.bpm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label bpm;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ h34101096_practice_6_1/h34101096_practice_6_1/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace h34101096_practice_6_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Image = Image.FromFile(@"...\Debug\quarternote.png");
+            radioButton2.Image = Image.FromFile(@"...\Debug\quavers.png");
+            comboBox1.SelectedIndex = 0;
+            button1.BackColor = Color.LightGray;
+            button2.BackColor = Color.LightGray;
+            button3.BackColor = Color.LightGray;
+            button4.BackColor = Color.LightGray;
+            button5.BackColor = Color.LightGray;
+            button6.BackColor = Color.LightGray;
+            button7.BackColor = Color.LightGray;
+            button8.BackColor = Color.LightGray;
+            button11.BackColor = Color.LightGray;
+            button12.BackColor = Color.LightGray;
+        }
+
+
+        
+        
+        int choose = 4;
+        int style = 1;
+        int sound = 0;
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            style = 1;
+            double k = trackBar1.Value * style / 60.0;
+            double j = 1000.0 / k;
+            timer1.Interval = Convert.ToInt32(j);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            style = 2;
+            double k = trackBar1.Value * style / 60.0;
+            double j = 1000.0 / k;
+            timer1.Interval = Convert.ToInt32(j);
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            bpm.Text = trackBar1.Value.ToString() + " BPM";
+            double k = trackBar1.Value * style / 60.0;
+            double j = 1000.0 / k;
+            timer1.Interval = Convert.ToInt32(j);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                choose = 4;
+                reset();
+                button11.Visible = true;
+                button12.Visible = true;
+
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Visible = false;
+                button5.Visible = false;
+                button6.Visible = false;
+                button7.Visible = false;
+                
+            }
+            else
+            {
+                choose = 8;
+                reset();
+                button2.Visible = true;
+                button3.Visible = true;
+                button4.Visible = true;
+                button5.Visible = true;
+                button6.Visible = true;
+                button7.Visible = true;
+
+                button11.Visible = false;
+                button12.Visible = false;
+                
+            }
+            double k = trackBar1.Value * style / 60.0;
+            double j = 1000.0 / k;
+            timer1.Interval = Convert.ToInt32(j);
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (start.Text == "Start")
+            {
+                start.Text = "Stop";
+
+                if (radioButton1.Checked == true)
+                {
+                    style = 1;
+                }
+                else
+                {
+                    style = 2;
+                }
+
+                timer1.Enabled = true;
+
+                double k = trackBar1.Value * style / 60.0;
+                double j = 1000.0 / k;
+                timer1.Interval = Convert.ToInt32(j);
+            }
+            else
+            {
+                start.Text = "Start";
+                timer1.Enabled = false;
+            }
+        }
+
+        private void reset()
+        {
+            button1.BackColor = Color.LightGray;
+            button2.BackColor = Color.LightGray;
+            button3.BackColor = Color.LightGray;
+            button4.BackColor = Color.LightGray;
+            button5.BackColor = Color.LightGray;
+            button6.BackColor = Color.LightGray;
+            button7.BackColor = Color.LightGray;
+            button8.BackColor = Color.LightGray;
+            button11.BackColor = Color.LightGray;
+            button12.BackColor = Color.LightGray;
+        }
+
+        
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SoundPlayer player1 = new SoundPlayer();
+            String filename1 = @"ding.wav";
+            player1.SoundLocation = filename1;
+            player1.Load();
+
+            SoundPlayer player2 = new SoundPlayer();
+            String filename2 = @"dong.wav";
+            player2.SoundLocation = filename2;
+            player2.Load();
+
+            
+            SoundPlayer player3 = new SoundPlayer();
+            String filename3 = @"doo.wav";
+            player3.SoundLocation = filename3;
+            player3.Load();
+
+            if (style == 1 && choose == 4)
+            {
+                if (sound % 4 == 0)
+                {
+                    player1.Play();
+                    reset();
+                    button1.BackColor = Color.LightGreen;
+                }
+                else if (sound % 4 == 1)
+                {
+                    player2.Play();
+                    reset();
+                    button11.BackColor = Color.LightGreen;
+                }
+                else if (sound % 4 == 2)
+                {
+                    player2.Play();
+                    reset();
+                    button12.BackColor = Color.LightGreen;
+                }
+                else if (sound % 4 == 3)
+                {
+                    player2.Play();
+                    reset();
+                    button8.BackColor = Color.LightGreen;
+                }
+                sound++;
+            }
+            else if (style == 1 && choose == 8)
+            {
+                if (sound % 8 == 0)
+                {
+                    player1.Play();
+                    reset();
+                    button1.BackColor = Color.LightGreen;
+                }
+                else if (sound % 8 == 1)
+                {
+                    player2.Play();
+                    reset();
+                    button2.BackColor = Color.LightGreen;
+                }
+                else if (sound % 8 == 2)
+                {
+                    player2.Play();
+                    reset();
+                    button3.BackColor = Color.LightGreen;
+                }
+                else if (sound % 8 == 3)
+                {
+                    player2.Play();
+                    reset();
+                    button4.BackColor = Color.LightGreen;
+                }
+                else if (sound % 8 == 4)
+                {
+                    player2.Play();
+                    reset();
+                    button5.BackColor = Color.LightGreen;
+                }
+                else if (sound % 8 == 5)
+                {
+                    player2.Play();
+                    reset();
+                    button6.BackColor = Color.LightGreen;
+                }
+                else if (sound % 8 == 6)
+                {
+                    player2.Play();
+                    reset();
+                    button7.BackColor = Color.LightGreen;
+                }
+                else if (sound % 8 == 7)
+                {
+                    player2.Play();
+                    reset();
+                    button8.BackColor = Color.LightGreen;
+                }
+                sound++;
+            }
+            else if (style == 2 && choose == 4)
+            {
+                if (sound % 8 == 0)
+                {
+                    player1.Play();
+                    reset();
+                    button1.BackColor = Color.LightGreen;
+                }
+                else if (sound % 8 == 1)
+                {
+                    player3.Play();
+                }
+                else if (sound % 8 == 2)
+                {
+                    player2.Play();
+                    reset();
+                    button11.BackColor = Color.LightGreen;
+                }
+                else if (sound % 8 == 3)
+                {
+                    player2.Play();
+                }
+                else if (sound % 8 == 4)
+                {
+                    player2.Play();
+                    reset();
+                    button12.BackColor = Color.LightGreen;
+                }
+                else if (sound % 8 == 5)
+                {
+                    player2.Play();
+                }
+                else if (sound % 8 == 6)
+                {
+                    player2.Play();
+                    reset();
+                    button8.BackColor = Color.LightGreen;
+                }
+                else if (sound % 8 == 7)
+                {
+                    player2.Play();
+                }
+                sound++;
+            }
+            else if (style == 2 && choose == 8)
+            {
+                if (sound % 16 == 0)
+                {
+                    player1.Play();
+                    reset();
+                    button1.BackColor = Color.LightGreen;
+                }
+                else if (sound % 16 == 2)
+                {
+                    player2.Play();
+                    reset();
+                    button2.BackColor = Color.LightGreen;
+                }
+                else if (sound % 16 == 4)
+                {
+                    player2.Play();
+                    reset();
+                    button3.BackColor = Color.LightGreen;
+                }
+                else if (sound % 16 == 6)
+                {
+                    player2.Play();
+                    reset();
+                    button4.BackColor = Color.LightGreen;
+                }
+                else if (sound % 16 == 8)
+                {
+                    player2.Play();
+                    reset();
+                    button5.BackColor = Color.LightGreen;
+                }
+                else if (sound % 16 == 10)
+                {
+                    player2.Play();
+                    reset();
+                    button6.BackColor = Color.LightGreen;
+                }
+                else if (sound % 16 == 12)
+                {
+                    player2.Play();
+                    reset();
+                    button7.BackColor = Color.LightGreen;
+                }
+                else if (sound % 16 == 14)
+                {
+                    player2.Play();
+                    reset();
+                    button8.BackColor = Color.LightGreen;
+                }
+                else if (sound % 16 == 1)
+                {
+                    player3.Play();
+                }
+                else
+                {
+                    player2.Play();
+                }
+                sound++;
+            }
+
+        }
+    }
+}
