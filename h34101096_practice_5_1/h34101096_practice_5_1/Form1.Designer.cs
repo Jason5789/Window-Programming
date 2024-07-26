8543148866e51c60d2ee0dfc805480fe8f6a285f
Jason5789 <109019020+Jason5789@users.noreply.github.com>
2024-07-26 21:47:47 +0800

    Add files via upload

---
+++ h34101096_practice_5_1/h34101096_practice_5_1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace h34101096_practice_5_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.answer = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.time = new System.Windows.Forms.Label();
+            this.wrong = new System.Windows.Forms.Label();
+            this.guess = new System.Windows.Forms.Label();
+            this.wrongTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeNum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(282, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 74);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "猜單字遊戲\r\n6次猜錯機會";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(333, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "請輸入要猜的單字:";
+            // 
+            // answer
+            // 
+            this.answer.Font = new System.Drawing.Font("新細明體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.answer.Location = new System.Drawing.Point(336, 213);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(145, 28);
+            this.answer.TabIndex = 2;
+            this.answer.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.answer_KeyPress);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(336, 287);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(145, 64);
+            this.start.TabIndex = 3;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Enabled = false;
+            this.time.Location = new System.Drawing.Point(667, 76);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(41, 15);
+            this.time.TabIndex = 4;
+            this.time.Text = "時間:";
+            this.time.Visible = false;
+            // 
+            // wrong
+            // 
+            this.wrong.AutoSize = true;
+            this.wrong.Enabled = false;
+            this.wrong.Location = new System.Drawing.Point(667, 107);
+            this.wrong.Name = "wrong";
+            this.wrong.Size = new System.Drawing.Size(71, 15);
+            this.wrong.TabIndex = 5;
+            this.wrong.Text = "猜錯次數:";
+            this.wrong.Visible = false;
+            // 
+            // guess
+            // 
+            this.guess.Enabled = false;
+            this.guess.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.guess.Location = new System.Drawing.Point(87, 306);
+            this.guess.Name = "guess";
+            this.guess.Size = new System.Drawing.Size(636, 74);
+            this.guess.TabIndex = 6;
+            this.guess.Text = "label5";
+            this.guess.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.guess.Visible = false;
+            // 
+            // wrongTime
+            // 
+            this.wrongTime.AutoSize = true;
+            this.wrongTime.Enabled = false;
+            this.wrongTime.Location = new System.Drawing.Point(744, 107);
+            this.wrongTime.Name = "wrongTime";
+            this.wrongTime.Size = new System.Drawing.Size(14, 15);
+            this.wrongTime.TabIndex = 7;
+            this.wrongTime.Text = "0";
+            this.wrongTime.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeNum
+            // 
+            this.timeNum.AutoSize = true;
+            this.timeNum.Enabled = false;
+            this.timeNum.Location = new System.Drawing.Point(711, 76);
+            this.timeNum.Name = "timeNum";
+            this.timeNum.Size = new System.Drawing.Size(14, 15);
+            this.timeNum.TabIndex = 8;
+            this.timeNum.Text = "0";
+            this.timeNum.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.timeNum);
+            this.Controls.Add(this.wrongTime);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.guess);
+            this.Controls.Add(this.wrong);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox answer;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Label wrong;
+        private System.Windows.Forms.Label guess;
+        private System.Windows.Forms.Label wrongTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timeNum;
+    }
+}
+
---
+++ h34101096_practice_5_1/h34101096_practice_5_1/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace h34101096_practice_5_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Button[] b = new Button[26];
+        string[] ans;
+        int[] correct;
+        int sec=0;
+        int finish = 0;
+        int sta = 0;
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            sta = 1;
+            answer.Text = answer.Text.ToUpper();
+            ans = new string[answer.Text.Length];
+            correct = new int[answer.Text.Length];
+
+            for (int i =0;i< answer.Text.Length; i++)
+            {
+                ans[i] = answer.Text.Substring(i, 1);
+                correct[i] = 0;
+            }
+
+            label1.Enabled = false;
+            label1.Visible = false;
+            label2.Enabled = false;
+            label2.Visible = false;
+            answer.Enabled = false;
+            answer.Visible = false;
+            start.Enabled = false;
+            start.Visible = false;
+
+            time.Enabled = true;
+            time.Visible = true;
+            timeNum.Enabled = true;
+            timeNum.Visible = true;
+            wrong.Enabled = true;
+            wrong.Visible = true;
+            guess.Enabled = true;
+            guess.Visible = true;
+            wrongTime.Enabled = true;
+            wrongTime.Visible = true;
+
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                int i = c - 65;
+                b[i] = new Button();
+                b[i].SetBounds(45*(i%9)+95, 45 * (i / 9) + 30, 40, 40);
+                b[i].BackColor = Color.White;
+                b[i].Enabled = false;
+                b[i].Text = c.ToString();
+                b[i].Name = c.ToString();
+                Controls.Add(b[i]);
+            }
+
+            int length = answer.Text.Length;
+            string s = "";
+
+            for(int i = 0; i < length; i++)
+            {
+                s += "_";
+
+                if (i != length - 1)
+                {
+                    s += " ";
+                }
+            }
+
+            guess.Text = s;
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (sta == 1)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.A:
+                        B_click("A");
+                        break;
+                    case Keys.B:
+                        B_click("B");
+                        break;
+                    case Keys.C:
+                        B_click("C");
+                        break;
+                    case Keys.D:
+                        B_click("D");
+                        break;
+                    case Keys.E:
+                        B_click("E");
+                        break;
+                    case Keys.F:
+                        B_click("F");
+                        break;
+                    case Keys.G:
+                        B_click("G");
+                        break;
+                    case Keys.H:
+                        B_click("H");
+                        break;
+                    case Keys.I:
+                        B_click("I");
+                        break;
+                    case Keys.J:
+                        B_click("J");
+                        break;
+                    case Keys.K:
+                        B_click("K");
+                        break;
+                    case Keys.L:
+                        B_click("L");
+                        break;
+                    case Keys.M:
+                        B_click("M");
+                        break;
+                    case Keys.N:
+                        B_click("N");
+                        break;
+                    case Keys.O:
+                        B_click("O");
+                        break;
+                    case Keys.P:
+                        B_click("P");
+                        break;
+                    case Keys.Q:
+                        B_click("Q");
+                        break;
+                    case Keys.R:
+                        B_click("R");
+                        break;
+                    case Keys.S:
+                        B_click("S");
+                        break;
+                    case Keys.T:
+                        B_click("T");
+                        break;
+                    case Keys.U:
+                        B_click("U");
+                        break;
+                    case Keys.V:
+                        B_click("V");
+                        break;
+                    case Keys.W:
+                        B_click("W");
+                        break;
+                    case Keys.X:
+                        B_click("X");
+                        break;
+                    case Keys.Y:
+                        B_click("Y");
+                        break;
+                    case Keys.Z:
+                        B_click("Z");
+                        break;
+                }
+            }
+            
+        }
+
+
+        private void B_click(string p)
+        {
+            int now;
+            int yes = 0;
+            for(now = 0; now < 26; now++)
+            {
+                if (p == b[now].Text)
+                {
+                    yes = 1;
+                    break;
+                }
+            }
+
+            if (answer.Text.Contains(p) && yes==1 && b[now].Visible == true)
+            {
+                string s="";
+                b[now].BackColor = Color.LightGreen;
+
+                for(int i = 0; i < ans.Length; i++)
+                {
+                    if (correct[i] == 1)
+                    {
+                        s += ans[i];
+                    }
+                    else if (ans[i] == b[now].Text)
+                    {
+                        s += b[now].Text;
+                        correct[i] = 1;
+                        finish++;
+                    }
+                    else
+                    {
+                        s += "_";
+                    }
+
+                    if (i != ans.Length - 1)
+                    {
+                        s += " ";
+                    }
+                }
+
+                guess.Text = s;
+
+                if (finish == ans.Length)
+                {
+                    timer1.Enabled = false;
+                    DialogResult d = MessageBox.Show("花費時間:" + timeNum.Text + "\n猜錯" + wrongTime.Text + "次", "You win!");
+
+                    if (d == DialogResult.OK)
+                    {
+                        reset();
+                    }
+                }
+            }
+            else if(b[now].Visible == true)
+            {
+                b[now].Enabled = false;
+                b[now].Visible = false;
+
+                int i = Convert.ToInt32(wrongTime.Text);
+                wrongTime.Text = (++i).ToString();
+
+                if (Convert.ToInt32(wrongTime.Text) == 6)
+                {
+                    timer1.Enabled = false;
+                    DialogResult d = MessageBox.Show("You Lose!");
+
+                    if (d == DialogResult.OK)
+                    {
+                        reset();
+                    }
+                }
+
+            }
+        }
+
+        private void answer_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char c = e.KeyChar;
+            if (((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) || c==8 || c==46)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sec++;
+            timeNum.Text = sec.ToString();
+        }
+
+        private void reset()
+        {
+            label1.Enabled = true;
+            label1.Visible = true;
+            label2.Enabled = true;
+            label2.Visible = true;
+            answer.Enabled = true;
+            answer.Visible = true;
+            start.Enabled = true;
+            start.Visible = true;
+
+            time.Enabled = false;
+            time.Visible = false;
+            timeNum.Enabled = false;
+            timeNum.Visible = false;
+            wrong.Enabled = false;
+            wrong.Visible = false;
+            guess.Enabled = false;
+            guess.Visible = false;
+            wrongTime.Enabled = false;
+            wrongTime.Visible = false;
+
+            answer.Text = "";
+
+            sec = 0;
+            finish = 0;
+            wrongTime.Text = "0";
+            timeNum.Text = "0";
+
+            for (int i = 0; i < 26; i++)
+            {
+                Controls.Remove(b[i]);
+            }
+        }
+
+    }
+}
