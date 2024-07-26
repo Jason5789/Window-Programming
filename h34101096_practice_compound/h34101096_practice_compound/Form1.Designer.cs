f8333388a902ca577c77b8d735c67b6a0bcc9a81
Jason5789 <109019020+Jason5789@users.noreply.github.com>
2024-07-26 21:55:16 +0800

    Add files via upload

---
+++ h34101096_practice_compound/h34101096_practice_compound/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace h34101096_practice_compound
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCapi = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Principal:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(392, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Years:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Rate(%):";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Info;
+            this.label4.Location = new System.Drawing.Point(121, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(273, 54);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Lable4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtCapi
+            // 
+            this.txtCapi.Location = new System.Drawing.Point(190, 66);
+            this.txtCapi.Name = "txtCapi";
+            this.txtCapi.Size = new System.Drawing.Size(100, 25);
+            this.txtCapi.TabIndex = 4;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(460, 66);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 25);
+            this.txtYear.TabIndex = 5;
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(190, 154);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(100, 25);
+            this.txtRate.TabIndex = 6;
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(395, 157);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(75, 23);
+            this.btnCal.TabIndex = 7;
+            this.btnCal.Text = "Calculate";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtCapi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCapi;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Button btnCal;
+    }
+}
+
---
+++ h34101096_practice_compound/h34101096_practice_compound/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace h34101096_practice_compound
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            double money = double.Parse(txtCapi.Text);//本金
+            double years = double.Parse(txtYear.Text);//年期
+            double yrate = double.Parse(txtRate.Text);//年利率
+            double total;//本利和
+            total = money * Math.Pow((1 + yrate / 100), years);
+            label4.Text = "accumulated amount= NT$ " + total.ToString("#,#.0") + " 元";
+            label4.Text += "\nTotal interest= NT$ " + (total - money).ToString("#,#.0") + " 元";//總利息
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = "";
+        }
+    }
+}
